04e84a4dbc5f977b2129d6cb7dc9719d6c853f61
Quincy Fer <quincy.ferr@gmail.com>
2025-04-22 15:40:10 +0200

    Cleanup main layout file. Created mainheader bg component

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,25 +1,30 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./MainHeader.module.css";
+import Image from "next/image";
+import MainHeaderBackGround from "./MainHeaderBackGround";
 
 const MainHeader = () => {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="Plate with Food In it" />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackGround />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="Plate with Food In it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
