a26ae24bf448c21c84d2713ff6a083c3b7f04c9b
Quincy Fer <quincy.ferr@gmail.com>
2025-04-22 15:01:18 +0200

    Add css modules for styling purposes

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
+import classes from "./MainHeader.module.css";
 
 const MainHeader = () => {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logoImg.src} alt="Plate with Food In it" />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
