8820f1f67b56caac0da6efa0ffcfa82b136dabba
Quincy Fer <quincy.ferr@gmail.com>
2025-04-22 14:34:29 +0200

    First Commit Foodies APp

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsSlug = ({ params }) => {
+  return (
+    <>
+      <p>{params.slug}</p>
+      <p></p>
+    </>
+  );
+};
+
+export default MealsSlug;
---
+++ app/meals/share/page.js
@@ -0,0 +1,10 @@
+const ShareMeals = () => {
+  return (
+    <>
+      <h1>Share Meals</h1>
+      <h2>lala</h2>
+    </>
+  );
+};
+
+export default ShareMeals;
