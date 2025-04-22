5880f6c01fbedcae4c03451804da2bfc0b89f3ea
Quincy Fer <quincy.ferr@gmail.com>
2025-04-22 16:29:03 +0200

    Create the standard meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 const MealsPage = () => {
-  return <h1>MealsPage</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, Created{" "}
+          <span className={classes.highlight}> by you</span>{" "}
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default MealsPage;
